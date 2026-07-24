3b492b01e5d988862c1d946b7a598219ead425ab
Shannon Mulloy <smulloy21@gmail.com>
2026-07-24 16:41:39 -0400

    get the mongo uri pw out of my logs

--- models/note.js
+++ models/note.js
@@ -4,7 +4,7 @@ mongoose.set('strictQuery', false);
 
 const url = process.env.MONGODB_URI;
 
-console.log('connecting to', url);
+console.log('connecting to mongo');
 mongoose.connect(url, { family: 4 })
   .then(result => {
     console.log('connected to MongoDB');
