5189da7801a4485ab0ddd33583b496564e22b3f5
Shannon Mulloy <smulloy21@gmail.com>
2026-07-25 16:21:13 -0400

    add validation for content field

--- index.js
+++ index.js
@@ -52,7 +52,7 @@ app.put('/api/notes/:id', (request, response, next) => {
     .catch(error => next(error));
 });
 
-app.post('/api/notes', (request, response) => {
+app.post('/api/notes', (request, response, next) => {
   const body = request.body;
 
   if (!body.content) {
@@ -68,7 +68,7 @@ app.post('/api/notes', (request, response) => {
 
   note.save().then(savedNote => {
     response.json(savedNote);
-  });
+  }).catch(error => next(error));
 });
 
 app.delete('/api/notes/:id', (request, response, next) => {
@@ -90,6 +90,8 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' });
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message });
   }
 
   next(error);
