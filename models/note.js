19fae80e0b9f2387ee68a203279e582ed5704ef8
Shannon Mulloy <smulloy21@gmail.com>
2026-07-26 15:47:37 -0400

    restructure for testing

--- models/note.js
+++ models/note.js
@@ -1,23 +1,10 @@
 const mongoose = require('mongoose')
 
-mongoose.set('strictQuery', false)
-
-const url = process.env.MONGODB_URI
-
-console.log('connecting to mongo')
-mongoose.connect(url, { family: 4 })
-  .then(() => {
-    console.log('connected to MongoDB')
-  })
-  .catch(error => {
-    console.log('error connecting to MongoDB:', error.message)
-  })
-
 const noteSchema = new mongoose.Schema({
   content: {
     type: String,
-    minLength: 5,
-    required: true
+    required: true,
+    minlength: 5
   },
   important: Boolean,
 })
