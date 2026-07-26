d4691807bb416651dd095b4dfb38df0e65289c36
Shannon Mulloy <smulloy21@gmail.com>
2026-07-26 17:19:47 -0400

    complete integration tests

--- tests/note_api.test.js
+++ tests/note_api.test.js
@@ -10,10 +10,7 @@ const api = supertest(app)
 
 beforeEach(async () => {
   await Note.deleteMany({})
-  let noteObject = new Note(helper.initialNotes[0])
-  await noteObject.save()
-  noteObject = new Note(helper.initialNotes[1])
-  await noteObject.save()
+  await Note.insertMany(helper.initialNotes)
 })
 
 test('notes are returned as json', async () => {
@@ -68,6 +65,53 @@ test('note without content is not added', async () => {
   const notesAtEnd = await helper.notesInDb()
 
   assert.strictEqual(notesAtEnd.length, helper.initialNotes.length)
+})
+
+test('a specific note can be viewed', async () => {
+  const notesAtStart = await helper.notesInDb()
+  const noteToView = notesAtStart[0]
+
+  const resultNote = await api
+    .get(`/api/notes/${noteToView.id}`)
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+
+  assert.deepStrictEqual(resultNote.body, noteToView)
+})
+
+test('a note can be deleted', async () => {
+  const notesAtStart = await helper.notesInDb()
+  const noteToDelete = notesAtStart[0]
+
+  await api
+    .delete(`/api/notes/${noteToDelete.id}`)
+    .expect(204)
+
+  const notesAtEnd = await helper.notesInDb()
+
+  const ids = notesAtEnd.map(n => n.id)
+  assert(!ids.includes(noteToDelete.id))
+
+  assert.strictEqual(notesAtEnd.length, helper.initialNotes.length - 1)
+})
+
+test('a note can be updated', async () => {
+  const notesAtStart = await helper.notesInDb()
+  const noteToUpdate = notesAtStart[0]
+
+  noteToUpdate.content = 'New Content'
+
+  await api
+    .put(`/api/notes/${noteToUpdate.id}`)
+    .send(noteToUpdate)
+    .expect(200)
+
+  const resultNote = await api
+    .get(`/api/notes/${noteToUpdate.id}`)
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+
+  assert.deepStrictEqual(resultNote.body, noteToUpdate)
 })
 
 after(async () => {
